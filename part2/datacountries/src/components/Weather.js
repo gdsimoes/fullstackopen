ea9b2371e2679ea98247be4abde2de683e676bf0
Guilherme Dias Simões <gds.simoes@gmail.com>
2021-05-26 16:31:47 -0400

    Clean up datacountries

--- part2/datacountries/src/components/Weather.js
+++ part2/datacountries/src/components/Weather.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-// console.log(process.env.REACT_APP_API_KEY);
-
 const Weather = ({ city }) => {
     const [weather, setWeather] = useState({});
 
--- part2/datacountries/src/index.js
+++ part2/datacountries/src/index.js
@@ -1,9 +1,4 @@
 import ReactDOM from "react-dom";
 import App from "./App";
-// import axios from "axios";
-
-// axios
-//     .get("https://restcountries.eu/rest/v2/all")
-//     .then((res) => res.data.forEach((x) => console.log(x.name)));
 
 ReactDOM.render(<App />, document.getElementById("root"));
