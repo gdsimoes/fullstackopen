467f61640b67b1d052529507e606ac44811fd5db
Guilherme Dias Simoes <gds.simoes@gmail.com>
2022-04-20 19:37:37 -0400

    Redo exercise 2.1

--- part2/datacountries/src/components/ShowButton.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const ShowButton = ({ setQuery, name }) => {
-    const handleClick = () => setQuery(name);
-    return <button onClick={handleClick}>show</button>;
-};
-
-export default ShowButton;
--- part2/phonebook/src/components/handleChange.js
+++
@@ -1,9 +0,0 @@
-// This function is useful in forms. Since it is such
-// a simple function maybe I should have used arrow
-// functions in the attributes.
-
-function handleChange(setFunction) {
-    return (event) => setFunction(event.target.value);
-}
-
-export default handleChange;
