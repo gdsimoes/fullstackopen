f735585c4083db88985dde5c0bc7f5511cd4b654
Guilherme Dias Simoes <gds.simoes@gmail.com>
2024-09-05 15:17:20 -0400

    Add solution to exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1234567" }]);
     const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState("");
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -12,7 +13,7 @@ const App = () => {
             return;
         }
 
-        const newPersons = [...persons, { name: newName }];
+        const newPersons = [...persons, { name: newName, number: newNumber }];
         setPersons(newPersons);
     }
 
@@ -26,12 +27,19 @@ const App = () => {
                     </label>
                 </div>
                 <div>
+                    <label>
+                        number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} />
+                    </label>
+                </div>
+                <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
             {persons.map((person) => (
-                <div key={person.name}>{person.name}</div>
+                <div key={person.name}>
+                    {person.name} {person.number}
+                </div>
             ))}
         </div>
     );
