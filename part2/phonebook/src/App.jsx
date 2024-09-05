d8f27a18b5ce8daa1ce16d500a1723482e5943f1
Guilherme Dias Simoes <gds.simoes@gmail.com>
2024-09-05 15:34:07 -0400

    Add solution to exercise 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,63 @@
 import { useState } from "react";
 
+function Filter({ search, setSearch }) {
+    return (
+        <div>
+            <label>
+                filter shown with <input value={search} onChange={(event) => setSearch(event.target.value)} />
+            </label>
+        </div>
+    );
+}
+
+function PersonForm({ newName, setNewName, newNumber, setNewNumber, persons, setPersons }) {
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        if (persons.some((person) => person.name === newName)) {
+            alert(`${newName} is already added to phonebook`);
+            return;
+        }
+
+        const newPersons = [...persons, { name: newName, number: newNumber }];
+        setPersons(newPersons);
+        setNewName("");
+        setNewNumber("");
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>
+                    name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
+                </label>
+            </div>
+            <div>
+                <label>
+                    number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} />
+                </label>
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    );
+}
+
+function Persons({ persons, search }) {
+    return (
+        <>
+            {persons
+                .filter((person) => person.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+                .map((person) => (
+                    <div key={person.name}>
+                        {person.name} {person.number}
+                    </div>
+                ))}
+        </>
+    );
+}
+
 const App = () => {
     const [persons, setPersons] = useState([
         { name: "Arto Hellas", number: "040-123456", id: 1 },
@@ -11,50 +69,26 @@ const App = () => {
     const [newNumber, setNewNumber] = useState("");
     const [search, setSearch] = useState("");
 
-    function handleSubmit(event) {
-        event.preventDefault();
-
-        if (persons.some((person) => person.name === newName)) {
-            alert(`${newName} is already added to phonebook`);
-            return;
-        }
-
-        const newPersons = [...persons, { name: newName, number: newNumber }];
-        setPersons(newPersons);
-    }
-
     return (
         <div>
             <h2>Phonebook</h2>
-            <div>
-                <label>
-                    filter shown with <input value={search} onChange={(event) => setSearch(event.target.value)} />
-                </label>
-            </div>
+
+            <Filter search={search} setSearch={setSearch} />
+
             <h2>add a new</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>
-                        name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} />
-                    </label>
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
+
+            <PersonForm
+                newName={newName}
+                setNewName={setNewName}
+                newNumber={newNumber}
+                setNewNumber={setNewNumber}
+                persons={persons}
+                setPersons={setPersons}
+            />
+
             <h2>Numbers</h2>
-            {persons
-                .filter((person) => person.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
-                .map((person) => (
-                    <div key={person.name}>
-                        {person.name} {person.number}
-                    </div>
-                ))}
+
+            <Persons persons={persons} search={search} />
         </div>
     );
 };
