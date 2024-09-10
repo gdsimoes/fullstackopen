47c2f14de81c7d9704adbc1fa25c1dd899723a42
Guilherme Dias Simoes <gds.simoes@gmail.com>
2024-09-10 11:54:12 -0400

    Change link

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "http://localhost:3001/api/persons";
 
 async function create(newPerson) {
     const response = await axios.post(baseUrl, newPerson);
